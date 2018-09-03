eea9a7497dfafd68c3e87b8c7d99f0469477ac3f
sammarjaved49@gmail.com <Ss7798472>
2018-09-04 00:44:12 +0200

    First Commit: Spring boot Application Project Setup

---
+++ src/main/java/de/ude/openlap/xapi/OpenLapXApiApplication.java
@@ -0,0 +1,12 @@
+package de.ude.openlap.xapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenLapXApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenLapXApiApplication.class, args);
+	}
+}
---
+++ src/test/java/de/ude/openlap/xapi/OpenLapXApiApplicationTests.java
@@ -0,0 +1,16 @@
+package de.ude.openlap.xapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenLapXApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
