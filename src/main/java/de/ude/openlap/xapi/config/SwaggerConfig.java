778b3a7f590c53c37157497dbd59f0e43055da3d
sammarjaved49@gmail.com <Ss7798472>
2018-09-24 00:12:08 +0200

    Refactor:Added contact info to swagger api doc

--- src/main/java/de/ude/openlap/xapi/config/SwaggerConfig.java
+++ src/main/java/de/ude/openlap/xapi/config/SwaggerConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,15 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
 				.select().apis(RequestHandlerSelectors.basePackage("de.ude.openlap.xapi.controller"))
 				.paths(regex("/v1.*"))
-                .build();
+				.build().apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		ApiInfo apiInfo = new ApiInfo("Spring Boot REST API", "Spring Boot REST API for Online Store", "1.0",
+				"Terms of service",
+				new Contact("Sammar Javed", "https://github.com/samjaved", "sammar.javed@stud.uni-due.de"),
+				"Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+		return apiInfo;
     }
+
 }
