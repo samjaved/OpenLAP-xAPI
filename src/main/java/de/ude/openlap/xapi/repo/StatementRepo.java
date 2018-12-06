2670d25badd6bacbee722b14c103dc2639540806
sammarjaved49@gmail.com <Ss7798472>
2018-12-06 04:26:26 +0100

    Feature: Added controller,rep and dto for Customized query.This QueryParametes can handle all kind of quries.

--- src/main/java/de/ude/openlap/xapi/repo/StatementRepo.java
+++ src/main/java/de/ude/openlap/xapi/repo/StatementRepo.java
@@ -5,13 +5,19 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import com.mongodb.DBObject;
+
 import de.ude.openlap.xapi.dto.Verb;
 import de.ude.openlap.xapi.model.Statement;
 
 public interface StatementRepo extends MongoRepository<Statement, String> {
 
 
-	@Query(value = "{'?0.?1':{ $exists: true  } }", fields = "{ '?0.?1':1}")
-	List<Verb> findAllVerbs(String objectname, String url);
+	@Query(value = "{'statement.verb':{ $exists: true  } }", fields = "{ 'id':0,'statement.verb':1}")
+	List<Verb> findAllVerbs();
+
+	@Query(value = "{'$and':[{'$or':?0},{'$and':?1}]}", fields = "?2")
+	List<Statement> findDataByCustomQuery(DBObject queryOptionalObject, DBObject queryComplusoryObject,
+			DBObject parametersToReceiveObject);
 
 }
