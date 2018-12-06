d1bc7a9ba1168b7d413c9ea2759588fe8c476527
sammarjaved49@gmail.com <Ss7798472>
2018-12-06 03:38:04 +0100

    Feature:Adding Jwt token as Bearer token in swagger

--- src/main/java/de/ude/openlap/xapi/config/SwaggerConfig.java
+++ src/main/java/de/ude/openlap/xapi/config/SwaggerConfig.java
@@ -2,11 +2,14 @@ package de.ude.openlap.xapi.config;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -20,7 +23,11 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
 				.select().apis(RequestHandlerSelectors.basePackage("de.ude.openlap.xapi.controller"))
 				.paths(regex("/v1.*"))
-				.build().apiInfo(metaData());
+				.build().apiInfo(metaData()).securitySchemes(Arrays.asList(apiKey()));
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("Authorization", "api_key", "header");
 	}
 
 	private ApiInfo metaData() {
