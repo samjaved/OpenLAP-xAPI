9ceeb1bd627a986dcded95adf257afc47c8ac99f
sammarjaved49@gmail.com <Ss7798472>
2019-01-03 06:42:09 +0100

    Feat: Changed the Activity Controller and Repository.
    Description:
     Added the function to get all activity data and convert in to OpenLAp
    data set.and the second function to receive the extension context
    parameter and retriving all possible values.

--- src/main/java/de/ude/openlap/xapi/controller/ActivityController.java
+++ src/main/java/de/ude/openlap/xapi/controller/ActivityController.java
@@ -1,9 +1,14 @@
 package de.ude.openlap.xapi.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import org.bson.types.ObjectId;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +16,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
+import de.rwthaachen.openlap.dataset.OpenLAPColumnDataType;
+import de.rwthaachen.openlap.dataset.OpenLAPDataSet;
+import de.rwthaachen.openlap.exceptions.OpenLAPDataColumnException;
+import de.ude.openlap.xapi.dto.OpenLapDataConverter;
+import de.ude.openlap.xapi.model.Activitiy;
 import de.ude.openlap.xapi.repo.ActivitiyRepo;
 
 @RestController
@@ -23,98 +32,144 @@ public class ActivityController {
 	@Autowired
 	private ActivitiyRepo activityRepo;
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	@ResponseBody
-	public String getAllActivities() throws IOException {
 
-		Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-		String activiteslist = gson.toJson(activityRepo.findAll());
-		String value = new String(activiteslist);
-		return value;
+
+	@PreAuthorize("hasRole('site_admin')")
+	@RequestMapping(value = "/show/activites", method = RequestMethod.GET)
+	@ResponseBody
+	public OpenLAPDataSet getActivities(@RequestParam("OrganizationId") ObjectId OrganizationId,
+			@RequestParam("LrsId") ObjectId lrsId) throws IOException, JSONException, OpenLAPDataColumnException {
+
+		ArrayList<Object> listOfActivityTypes = new ArrayList<Object>();
+		ArrayList<Object> listOfActivityNames = new ArrayList<Object>();
+		ArrayList<String> listOfActivityIds = new ArrayList<String>();
+		ArrayList<Object> listOfActivityDescription = new ArrayList<Object>();
+		ArrayList<String> listOfActivityExtensionIds = new ArrayList<String>();
+		ArrayList<String> listOfActivityExtensionContextKeys = new ArrayList<String>();
+		for (Activitiy activity : activityRepo.findActivitiesByOrganizationAndLrs(OrganizationId, lrsId)) {
+			if (activity.getType() != null) {
+				listOfActivityTypes.add(activity.getType());
+			}
+			if (activity.getActivityId() != null) {
+				listOfActivityIds.add(activity.getActivityId());
+			}
+
+			if (activity.getName() != null) {
+			String activityName = new Gson().toJson(activity.getName());
+			JSONObject activityNameObject = new JSONObject(activityName);
+				Iterator<?> displaykey = activityNameObject.keys();
+
+			while (displaykey.hasNext()) {
+				// loop to get the dynamic key
+				String DynamicLanguageKey = (String) displaykey.next();
+
+				// get the value of the dynamic key
+				if (!listOfActivityNames.contains(activityNameObject.get(DynamicLanguageKey))) {
+					listOfActivityNames.add(activityNameObject.get(DynamicLanguageKey));
+				}
+
+			}
+			}
+			if (activity.getDescription() != null) {
+			String activityDescription = new Gson().toJson(activity.getDescription());
+
+			JSONObject activityDescriptionObject = new JSONObject(activityDescription);
+				Iterator<?> descriptionKey = activityDescriptionObject.keys();
+
+			while (descriptionKey.hasNext()) {
+				// loop to get the dynamic key
+				String DynamicdescriptionKey = (String) descriptionKey.next();
+
+				// get the value of the dynamic key
+				if (!listOfActivityDescription.contains(activityDescriptionObject.get(DynamicdescriptionKey))) {
+					listOfActivityDescription.add(activityDescriptionObject.get(DynamicdescriptionKey));
+				}
+
+			}
+		}
+			if (activity.getExtensions() != null) {
+				String activityExtension = new Gson().toJson(activity.getExtensions());
+
+				JSONObject activityExtensionObject = new JSONObject(activityExtension);
+				Iterator<?> extentionKey = activityExtensionObject.keys();
+				while (extentionKey.hasNext()) {
+					// loop to get the dynamic key
+					String DynamicextentionKey = (String) extentionKey.next();
+
+					// get the value of the dynamic key
+					if (!listOfActivityExtensionIds.contains(DynamicextentionKey)) {
+						listOfActivityExtensionIds.add(DynamicextentionKey);
+					}
+					if (DynamicextentionKey != null) {
+						JSONObject contextualObject = activityExtensionObject.getJSONObject(DynamicextentionKey);
+						Iterator<?> contextKey = contextualObject.keys();
+						while (contextKey.hasNext()) {
+							// loop to get the dynamic key
+							String DynamiccontextKey = (String) contextKey.next();
+
+							// get the value of the dynamic key
+							if (!listOfActivityExtensionContextKeys.contains(DynamiccontextKey)) {
+								listOfActivityExtensionContextKeys.add(DynamiccontextKey);
+							}
+
+
+						}
+					}
+
+				}
+			}
+
+		}
+		OpenLapDataConverter dataConveter = new OpenLapDataConverter();
+		dataConveter.SetOpenLapDataColumn("ActivityTypes", OpenLAPColumnDataType.Text, true, listOfActivityTypes);
+		dataConveter.SetOpenLapDataColumn("ActivityNames", OpenLAPColumnDataType.Text, true, listOfActivityNames);
+		dataConveter.SetOpenLapDataColumn("ActivityIds", OpenLAPColumnDataType.Text, true, listOfActivityIds);
+		dataConveter.SetOpenLapDataColumn("ActivityDescription", OpenLAPColumnDataType.Text, true,
+				listOfActivityDescription);
+
+		dataConveter.SetOpenLapDataColumn("ActivityExtentionIds", OpenLAPColumnDataType.Text, true,
+				listOfActivityExtensionIds);
+		dataConveter.SetOpenLapDataColumn("ActivityExtentionContextKeys", OpenLAPColumnDataType.Text, true,
+				listOfActivityExtensionContextKeys);
+
+
+		return dataConveter.getDataSet();
+
 
 	}
 
-	@RequestMapping(value = "/show", method = RequestMethod.GET)
+	@PreAuthorize("hasRole('site_admin')")
+	@RequestMapping(value = "/show/activitesExtensionContextValues", method = RequestMethod.GET)
 	@ResponseBody
-	public String getActivitiesById(@RequestParam("Id") String Id) throws IOException {
+	public OpenLAPDataSet getActivitiesExtensionContextValues(@RequestParam("OrganizationId") ObjectId OrganizationId,
+			@RequestParam("LrsId") ObjectId lrsId, @RequestParam("extensionId") String extensionId,
+			@RequestParam("extensionContextKey") String extensionContextKey)
+			throws IOException, JSONException, OpenLAPDataColumnException {
 
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String activiteslist = gson.toJson(activityRepo.findById(Id));
-		return activiteslist;
+		ArrayList<Object> listOfExtensionContextValues = new ArrayList<Object>();
+		for (Activitiy activity : activityRepo.findContextualFieldValuesByExtensionUrlAndKey(OrganizationId, lrsId,
+				"extensions", extensionId, extensionContextKey)) {
+			if (activity.getExtensions() != null) {
+				String activityExtension = new Gson().toJson(activity.getExtensions());
+				JSONObject activityExtensionObject = new JSONObject(activityExtension);
+				if (!activityExtensionObject.toString().equals("{}")) {
+				JSONObject extensionIdObject = activityExtensionObject.getJSONObject(extensionId);
+				if (!listOfExtensionContextValues.contains(extensionIdObject.get(extensionContextKey))) {
+					listOfExtensionContextValues.add(extensionIdObject.get(extensionContextKey));
+				}
+				}
+			}
+		}
+
+		OpenLapDataConverter dataConveter = new OpenLapDataConverter();
+		dataConveter.SetOpenLapDataColumn(extensionContextKey, OpenLAPColumnDataType.Text, true,
+				listOfExtensionContextValues);
+
+		return dataConveter.getDataSet();
 
 	}
 
-	@RequestMapping(value = "/show/activitesByActivtyId", method = RequestMethod.GET)
-	@ResponseBody
-	public String getActivitiesByActivityId(@RequestParam("activityId") String activityId) throws IOException {
-		
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String activiteslist = gson.toJson(activityRepo.findByActivityId(activityId));
-		return activiteslist;
 
-
-	}
-
-	@RequestMapping(value = "/show/activitesByType", method = RequestMethod.GET)
-	@ResponseBody
-	public String getActivitiesByType(@RequestParam("type") String type) throws IOException {
-
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String activiteslist = gson.toJson(activityRepo.findByType(type));
-		return activiteslist;
-
-	}
-
-	@RequestMapping(value = "/show/activitesByObjectName/{objectName}", method = RequestMethod.GET)
-	@ResponseBody
-	public String getActivitiesByOBjectNameAndProperty(@PathVariable("objectName") String objcetName,
-			@RequestParam("propertyName") String propertyName, @RequestParam("propertyValue") String propertyValue)
-			throws IOException {
-
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String activiteslist = gson
-				.toJson(activityRepo.findByObjectNameAndProperty(objcetName, propertyName, propertyValue));
-		return activiteslist;
-
-	}
-
-	@RequestMapping(value = "/show/metadata/{objectName}", method = RequestMethod.GET)
-	@ResponseBody
-	public String getActivitiesByOBjectsName(@PathVariable("objectName") String objectname,
-			@RequestParam("secondObjectName") String secondobjectname,
-			@RequestParam("propertyName") String propertyName, @RequestParam("propertyValue") int propertyValue)
-			throws IOException {
-
-		Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-		String activiteslist = gson
-				.toJson(activityRepo.findByObjects(objectname, secondobjectname, propertyName, propertyValue));
-		return activiteslist;
-
-	}
-
-	@RequestMapping(value = "/show/extentions/metadata", method = RequestMethod.GET)
-	@ResponseBody
-	public String getActivitiesContextualFields(@RequestParam("objectName") String objectName,
-			@RequestParam("extensionsUrl") String extensionsUrl)
-			throws IOException {
-		Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-		String activiteslist = gson
-				.toJson(activityRepo.findContextualFieldsByExtensionUrl(objectName, extensionsUrl));
-		return activiteslist;
-	}
-
-	//@RequestMapping(value = "/show/extentions/metadata", method = RequestMethod.POST)
-	//@ResponseBody
-	/*public String getActivitiesByQuery(@RequestBody ExtentionParameters jsonString)
-			throws IOException {
-		System.out.println(jsonString.getObj1());
-		String query = "{'extensions.http://collide&46;info/xapi/gitAction.name':'gitExtension'}";
-		System.out.println(query);
-		Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-		String activiteslist = gson
-				.toJson(activityRepo.findActivitesByQuery(query));
-		//return activiteslist;
-	}*/
 
 
 }
--- src/main/java/de/ude/openlap/xapi/repo/ActivitiyRepo.java
+++ src/main/java/de/ude/openlap/xapi/repo/ActivitiyRepo.java
@@ -2,6 +2,7 @@ package de.ude.openlap.xapi.repo;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -9,22 +10,22 @@ import de.ude.openlap.xapi.model.Activitiy;
 
 public interface ActivitiyRepo extends MongoRepository<Activitiy, String> {
 
-	@Query(value = "{ '?0.?1' : '?2' }")
-	List<Activitiy> findByObjectNameAndProperty(String Object, String Parameter, String Value);
-
-	@Query(value = "{ '?0.?1.?2' : '?3' }")
-	List<Activitiy> findByObjects(String Object, String Secondobject, String Parameter, int Value);
-
-	List<Activitiy> findByActivityId(String activityid);
-
-	@Query(value = "{'?0.?1':{ $exists: true } }", fields = "{ 'extensions':1}")
-	List<Activitiy> findContextualFieldsByExtensionUrl(String objectname, String url);
-
 	/*
-	 * @Query(value = "{?0}") List<Activitiy> findActivitesByQuery(String query);
+	 * @Query(value = "{ '?0.?1' : '?2' }") List<Activitiy>
+	 * findByObjectNameAndProperty(String Object, String Parameter, String Value);
+	 * 
+	 * @Query(value = "{ '?0.?1.?2' : '?3' }") List<Activitiy> findByObjects(String
+	 * Object, String Secondobject, String Parameter, int Value);
+	 * 
+	 * List<Activitiy> findByActivityId(String activityid);
 	 */
 
-	List<Activitiy> findByType(String type);
+	@Query(value = "{'$and':[{'organisation':?0},{'lrs_id':?1}]}", fields = "{ '?2.?3.?4':1}")
+	List<Activitiy> findContextualFieldValuesByExtensionUrlAndKey(ObjectId organizationId, ObjectId lrsId,
+			String extension, String extensionId, String extensionContextKey);
+
+	@Query(value = "{'$and':[{'organisation':?0},{'lrs_id':?1}]}")
+	List<Activitiy> findActivitiesByOrganizationAndLrs(ObjectId organizationId, ObjectId lrsId);
 
 
 }
